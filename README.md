f84c4869f891965d4928ce81012a076c0a74dfbe
gugu256 <gugu256@mail.com>
2023-07-09 20:01:21 +0200

    Added README

---
+++ README.md
@@ -0,0 +1,7 @@
+# Flask Template
+
+The simplest flask template possible
+
+Just clone it and use it
+
+`git clone https://github.com/gugu256/Flask-Template.git`
--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 from flask import Flask
+
 app = Flask('')
 
 @app.route('/')
 def home():
-    return "AY0"
+    return "Hello, World!"
 
-def run():
-  app.run(host='0.0.0.0',port=8080)
-
-run()
+app.run(host='0.0.0.0',port=8080)
